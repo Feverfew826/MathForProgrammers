808a0f5534738f3dee72b1412442776c2778c31f
Jeong Hwansu <lightkill826@gmail.com>
2025-03-25 23:07:14 +0900

    7문제 품.
    답안이 점점 길어지고 있다는 생각이 듦.

--- Chapter6/Chapter6.py
+++ Chapter6/Chapter6.py
@@ -1,11 +1,14 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
 from Vector import Vector
 from random import uniform
-from math import isclose
+from math import isclose, sin
 from CarForSale import CarForSale
-from json import loads, dumps
+from json import loads
 from pathlib import Path
-from datetime import datetime, timedelta
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+from random import randint
 
     
 class Vec2(Vector):
@@ -157,6 +160,150 @@ def A9():
         u, v, w = cars[i], cars[i+1], cars[i+2]
         testWithZero(CarForSale.zero(), approx_equal_car,  a, b, u, v, w)
 
+def plot(fs, xmin, xmax):
+    xs = np.linspace(xmin,xmax,100)
+    fig, ax = plt.subplots()
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    for f in fs:
+        ys = [f(x) for x in xs]
+        plt.plot(xs,ys)
+    plt.show()
+class Function(Vector):
+    def __init__(self, function):
+        self.function = function
+    def add(self, other):
+        def addedFunction(param):
+            return self(param) + other(param)
+        return Function(addedFunction)
+    def scale(self, scalar):
+        def scaledFunction(param):
+            return self(param) * scalar
+        return Function(scaledFunction)
+    @classmethod
+    def zero():
+        def zeroFunction(param):
+            return 0
+        return Function(zeroFunction)
+    def __call__(self, args):
+        return self.function(args)
+def A10():
+    def f_origin(x):
+        return 0.5 * x +3
+    def g_origin(x):
+        return sin(x)
+    f = Function(f_origin)
+    g = Function(g_origin)
+    plot([f,g,f+g,3*g], -10, 10)
+
+def A11():
+    print("방법이 떠오르지 않는다.")
+
+def A12():
+    def approx_equal_fuction(v, w):
+        allSame = True;
+        for i in range(0, 100):
+            x=random_scalar()
+            if(isclose(v(x))):
+                allSame=False
+                break
+        return allSame
+    def random_function():
+        degree = randint(0,5)
+        #p = Polynomial(*[uniform(-10,10) for _ in range(0,degree)])
+        #return Function(lambda x: p(x))
+    for i in range(0, 100):
+        a, b=random_scalar(), random_scalar()
+        u, v, w=random_function(), random_function(), random_function()
+    testWithZero(Function.zero, approx_equal_fuction, a, b, u, v, w)
+
+class Function2(Vector):
+    def __init__(self, function):
+        self.function = function
+    def add(self, other):
+        def addedFunction(param0, param1):
+            return self(param0, param1) + other(param0, param1)
+        return Function2(addedFunction)
+    def scale(self, scalar):
+        def scaledFunction(param0, param1):
+            return self(param0, param1) * scalar
+        return Function2(scaledFunction)
+    @classmethod
+    def zero():
+        def zeroFunction(param0, param1):
+            return 0
+        return Function2(zeroFunction)
+    def __call__(self, *args):
+        return self.function(args[0], args[1])
+def A13():
+    def f_origin(x, y):
+        return 0.5 * x +3 + 0.5 * y +3
+    def g_origin(x, y):
+        return sin(x+y)
+    f = Function2(f_origin)
+    g = Function2(g_origin)
+    x=1
+    y=2
+    print([f(x,y),g(x,y),(f+g)(x,y),(3*g)(x,y)])
+    x=4
+    y=5
+    print([f(x,y),g(x,y),(f+g)(x,y),(3*g)(x,y)])
+    x=-1
+    y=-4
+    print([f(x,y),g(x,y),(f+g)(x,y),(3*g)(x,y)])
+    
+def A14():
+    print("d")
+
+class Matrix(Vector):
+    @classmethod
+    def col(cls):
+        pass
+    @classmethod
+    def row(cls):
+        pass
+class Matrix5_by_3(Matrix):
+    @classmethod
+    def col(cls):
+        return 3
+    @classmethod
+    def row(cls):
+        return 5
+    def __init__(self, arg):
+        self.arg = arg
+    def add(self, other):
+        return Matrix5_by_3(tuple(tuple([pair[0] + pair[1] for pair in zip(rowPair[0], rowPair[1])]) for rowPair in zip(self.arg, other.arg)))
+    def scale(self, scalar):
+        return Matrix5_by_3(tuple(tuple(elmt*scalar for elmt in row) for row in self.arg))
+    def __repr__(self):
+        return self.arg.__repr__()
+    @classmethod
+    def zero(cls):
+        return Matrix5_by_3(tuple(tuple([0 for j in range(0,3)]) for i in range(0, 5)))
+def A15():
+    u=Matrix5_by_3(((1,2,3),(4,5,6),(7,8,9),(10, 11,12),(13,14,15)))
+    v=Matrix5_by_3(((16,17,18),(19,20,21),(22,23,24),(25,26,27),(28,29,30)))
+    print(u)
+    print(v)
+    print(u+v)
+    print(u*3)
+
+def A16():
+    def approx_equal_matrix5_by_3(u,v):
+        isSame = True
+        for row in range(0, 5):
+            for col in range(0,3):
+                if(isclose(u.arg[row][col], v.arg[row][col])==False):
+                    isSame=False
+                    break
+        return isSame
+    def random_matrix5_by_3():
+        return Matrix5_by_3(tuple(tuple([random_scalar() for j in range(0,3)]) for i in range(0, 5)))
+    for i in range(0, 100):
+        a, b = random_scalar(), random_scalar()
+        u, v, w = random_matrix5_by_3(), random_matrix5_by_3(), random_matrix5_by_3()
+        testWithZero(Matrix5_by_3.zero(), approx_equal_matrix5_by_3, a,b,u,v,w)
+
 #A1()
 #A2()
 #A3()
@@ -165,4 +312,11 @@ def A9():
 #A6()
 #A7()
 #A8()
-A9()
+#A9()
+#A10()
+#A11()
+#A12()
+#A13()
+#A14()
+#A15()
+A16()
